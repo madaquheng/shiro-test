185001f4f23bb95b526515a9ed827254b2ef5377
madaquheng <quheng@madadata.com>
2016-11-25 14:35:54 +0800

    init project

---
+++ src/main/java/MainTest.java
@@ -0,0 +1,14 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by quheng on 11/25/16.
+ */
+public class MainTest {
+    private static final transient Logger log = LoggerFactory.getLogger(MainTest.class);
+
+    public static void main(String[] args) {
+        log.info("My Test Apache Shiro Application");
+        System.exit(0);
+    }
+}
